e829d10f6dccb94d418bcd085df29cec291d4524
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-28 09:44:48 +0530

    Updates

---
+++ precache-manifest.c476ca86fb80ddb10a28f25841c6374a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c97a532e9766c974c453d699296505",
+    "url": "/Airbnb-Bharath/index.html"
+  },
+  {
+    "revision": "ad9b4750f231f5a529fb",
+    "url": "/Airbnb-Bharath/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "bfd908eac1b38235ecd1",
+    "url": "/Airbnb-Bharath/static/css/main.f42617f2.chunk.css"
+  },
+  {
+    "revision": "ad9b4750f231f5a529fb",
+    "url": "/Airbnb-Bharath/static/js/2.99aac85e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Airbnb-Bharath/static/js/2.99aac85e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfd908eac1b38235ecd1",
+    "url": "/Airbnb-Bharath/static/js/main.b03b7ed7.chunk.js"
+  },
+  {
+    "revision": "389cdf87af7053fc5d50",
+    "url": "/Airbnb-Bharath/static/js/runtime-main.9cb45d89.js"
+  }
+]);
